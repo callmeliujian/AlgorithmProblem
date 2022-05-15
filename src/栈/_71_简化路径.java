644b73bfbc7d1c435852600bb2007fbb499fd0ea
callmeliujian <tel.nelson@icloud.com>
2022-05-15 23:46:51 +0800

    Create _71_简化路径.java

---
+++ src/栈/_71_简化路径.java
@@ -0,0 +1,41 @@
+package 栈;
+
+import 字符串._402_移掉K位数字;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class _71_简化路径 {
+
+    public String simplifyPath(String path) {
+        String[] names = path.split("/");
+        Deque<String> stack = new ArrayDeque<>();
+        for (String str : names) {
+            if (str.equals("..")) {
+                if (!stack.isEmpty()) {
+                    stack.removeLast();
+                }
+            } else if (str.length() > 0 && !str.equals(".")) {
+                stack.addLast(str);
+            }
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        if (stack.isEmpty()) {
+            stringBuilder.append("/");
+        } else {
+            for (String str : stack) {
+                stringBuilder.append("/");
+                stringBuilder.append(str);
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+    public static void main(String args[]) {
+        String str1 = "/home/";
+        _71_简化路径 b = new _71_简化路径();
+        String index = b.simplifyPath(str1);
+        System.out.print(index);
+    }
+
+}
