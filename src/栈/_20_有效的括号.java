e8b3dcfe8c09d10cd65b340c852c1a1776b710d2
callmeliujian <tel.nelson@icloud.com>
2022-05-15 16:09:25 +0800

    Create _20_有效的括号.java

---
+++ src/栈/_20_有效的括号.java
@@ -0,0 +1,35 @@
+package 栈;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class _20_有效的括号 {
+    public boolean isValid(String s) {
+        char[] array = s.toCharArray();
+        Deque<Character> stack = new ArrayDeque<>();
+        for (int i = 0; i < array.length; i ++) {
+            char currentChar = array[i];
+            if (currentChar == '(' || currentChar == '[' || currentChar == '{') {
+                stack.addLast(currentChar);
+            } else {
+                if (stack.size() > 0) {
+                    char stackChar = stack.peekLast();
+                    if ((stackChar == '(' && currentChar == ')') || (stackChar == '[' && currentChar == ']')  || (stackChar == '{' && currentChar == '}') ) {
+                        stack.removeLast();
+                    } else {
+                        return false;
+                    }
+                } else {
+                    return false;
+                }
+
+            }
+        }
+        if (stack.size() == 0) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+}
