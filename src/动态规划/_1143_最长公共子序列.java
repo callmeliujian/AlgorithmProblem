3093667e8842dac6404dadcad9369ddaeda74eeb
callmeliujian <tel.nelson@icloud.com>
2022-05-27 23:13:09 +0800

    添加注释

--- src/动态规划/_1143_最长公共子序列.java
+++ src/动态规划/_1143_最长公共子序列.java
@@ -8,6 +8,11 @@ public class _1143_最长公共子序列 {
         return lcse(str1,str2);
     }
 
+    // 两个字符数组 str1[0...i] 和 str2[0...j]
+    // 1. 最长公共子序列可能既不已 str1[i] 也不已 str2[j] 结尾 例如 ab123c d123ef 其最长公共子序列为 123 -> dp[i - 1][j - 1]
+    // 2. 最长公共子序列可能以 str1[i] 结尾，不以 str2[j] 结尾 例如 ab123 c123ef 其最长公共子序列为 123 -> dp[i][j - 1]
+    // 3. 最长公共子序列不以 str1[i] 结尾，可能以 str2[j] 结尾 例如 ab123c cf123 其最长公共子序列为 123 -> dp[i - 1][j]
+    // 4. 最长公共子序列即可能以 str1[i] 结尾，又可能以 str2[j] 结尾 例如 ab12c3 d12ef3 存在条件为 str1[i] == str2[j] 其最长公共子序列为 123 -> dp[i - 1][j - 1] + 1
     private int lcse(char[] str1, char[] str2) {
         int[][] dp = new int[str1.length][str2.length];
         dp[0][0] = str1[0] == str2[0] ? 1:0;
