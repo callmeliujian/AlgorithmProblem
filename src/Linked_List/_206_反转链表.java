d52a9b482b6665b715d2aa2882dd0bf7f1b6dbbe
callmeliujian <tel.nelson@icloud.com>
2022-06-04 23:15:06 +0800

    Create _206_反转链表.java

---
+++ src/Linked_List/_206_反转链表.java
@@ -0,0 +1,16 @@
+package Linked_List;
+
+public class _206_反转链表 {
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode cur = head;
+        ListNode temp = null;
+        while (cur != null) {
+            temp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = temp;
+        }
+        return pre;
+    }
+}
