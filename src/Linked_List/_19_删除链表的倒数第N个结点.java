252156a6e76b86ddea33ffd3c0c976e5e8ae7484
callmeliujian <tel.nelson@icloud.com>
2022-06-05 00:33:25 +0800

    Create _19_删除链表的倒数第N个结点.java

---
+++ src/Linked_List/_19_删除链表的倒数第N个结点.java
@@ -0,0 +1,26 @@
+package Linked_List;
+
+public class _19_删除链表的倒数第N个结点 {
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if (head == null) {
+            return null;
+        }
+        if (head.next == null) {
+            return null;
+        }
+        ListNode dummyHead = new ListNode(0,head);
+        ListNode first = head;
+        ListNode second = dummyHead;
+        for (int i = 0; i < n; i++) {
+            first = first.next;
+        }
+        while (first != null) {
+            first = first.next;
+            second = second.next;
+        }
+        second.next = second.next.next;
+        return dummyHead.next;
+    }
+
+}
