7ac49be25d357fe7477dfb2ac7659ebe30e081ff
callmeliujian <tel.nelson@icloud.com>
2022-06-05 01:09:03 +0800

    Create _142环形链表_II.java

---
+++ src/Linked_List/_142环形链表_II.java
@@ -0,0 +1,26 @@
+package Linked_List;
+
+public class _142环形链表_II {
+
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                ListNode p = head;
+                while (p != slow) {
+                    p = p.next;
+                    slow = slow.next;
+                }
+                return p;
+            }
+        }
+        return null;
+    }
+
+}
