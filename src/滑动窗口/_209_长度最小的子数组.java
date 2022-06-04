e188e797d5ba013be791face97e9cf5de4212b0c
callmeliujian <tel.nelson@icloud.com>
2022-06-04 14:22:41 +0800

    Update _209_长度最小的子数组.java

--- src/滑动窗口/_209_长度最小的子数组.java
+++ src/滑动窗口/_209_长度最小的子数组.java
@@ -11,7 +11,9 @@ public class _209_长度最小的子数组 {
             if (sum >= target) {
                 min_len = Math.min(min_len, i - start + 1);
             }
-            while (sum >= target) {
+            while (sum >= target) { // 先判断  min_len = Math.min 的原因，用例：
+//                15
+//                [1,2,3,4,5]
                 min_len = Math.min(min_len,i - start + 1);
                 sum -= nums[start];
                 start++;
@@ -21,6 +23,20 @@ public class _209_长度最小的子数组 {
             return 0;
         }
         return min_len;
+    }
+
+    // 性能更高的解决方法
+    public int minSubArrayLen1(int s, int[] nums) {
+        int startIndex = 0, ans = Integer.MAX_VALUE, sum =0;
+        for (int i = 0; i < nums.length; i++) {
+            sum = sum + nums[i];
+            while (sum >= s) {
+                ans = Math.min(ans, i - startIndex + 1);
+                sum = sum - nums[startIndex];
+                startIndex++;
+            }
+        }
+        return ans == Integer.MAX_VALUE ? 0 : ans;
     }
 
 }
