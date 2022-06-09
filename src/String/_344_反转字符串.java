57a46008dd01fd9aa8e683ec33ac1f099e890ab5
callmeliujian <tel.nelson@icloud.com>
2022-06-09 20:12:14 +0800

    Create _344_反转字符串.java

---
+++ src/String/_344_反转字符串.java
@@ -0,0 +1,17 @@
+package String;
+
+public class _344_反转字符串 {
+
+    public void reverseString(char[] s) {
+        int l = 0;
+        int r = s.length - 1;
+        while (l < r) {
+            s[l] ^= s[r];
+            s[r] ^= s[l];
+            s[l] ^= s[r];
+            l++;
+            r--;
+        }
+    }
+
+}
