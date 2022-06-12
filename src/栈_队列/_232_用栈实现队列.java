ce79cf92f485db43e8f139d0933ba24354559253
LiuJian <tel.nelson@icloud.com>
2022-06-12 16:40:44 +0800

    Update _232_用栈实现队列.java

--- src/栈_队列/_232_用栈实现队列.java
+++ src/栈_队列/_232_用栈实现队列.java
@@ -10,47 +10,44 @@ public class _232_用栈实现队列 {
 
 class MyQueue {
 
-    Stack<Integer> stack1;
-    Stack<Integer> stack2;
+    Stack<Integer> stackIn;
+    Stack<Integer> stackOut;
 
     public MyQueue() {
-        stack1 = new Stack<>();
-        stack2 = new Stack<>();
+        stackIn = new Stack<>();
+        stackOut = new Stack<>();
     }
 
     public void push(int x) {
-        stack1.push(x);
+        stackIn.push(x);
     }
 
     public int pop() {
         int ans = 0;
-        while (!stack1.isEmpty()) {
-            ans = stack1.pop();
-            stack2.push(ans);
+        if (stackOut.isEmpty()) {
+            while (!stackIn.isEmpty()) {
+                ans = stackIn.pop();
+                stackOut.push(ans);
+            }
+            ans = stackOut.pop();
+            while (!stackOut.isEmpty()) {
+                int temp = stackOut.pop();
+                stackIn.push(temp);
+            }
+        } else {
+            ans = stackOut.pop();
         }
-        ans = stack2.pop();
-        while (!stack2.isEmpty()) {
-            int temp = stack2.pop();
-            stack1.push(temp);
-        }
+
         return ans;
     }
 
     public int peek() {
-        int ans = 0;
-        while (!stack1.isEmpty()) {
-            ans = stack1.pop();
-            stack2.push(ans);
-        }
-        ans = stack2.peek();
-        while (!stack2.isEmpty()) {
-            int temp = stack2.pop();
-            stack1.push(temp);
-        }
+        int ans = pop();
+        stackOut.push(ans);
         return ans;
     }
 
     public boolean empty() {
-        return stack1.isEmpty();
+        return stackIn.isEmpty() && stackOut.isEmpty();
     }
 }
