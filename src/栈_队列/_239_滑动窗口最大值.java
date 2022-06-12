39b359612f3443f020df7e2eeb6c8f398e4b83b5
LiuJian <tel.nelson@icloud.com>
2022-06-13 00:06:34 +0800

    Update _239_滑动窗口最大值.java

--- src/栈_队列/_239_滑动窗口最大值.java
+++ src/栈_队列/_239_滑动窗口最大值.java
@@ -5,10 +5,41 @@ package 栈_队列;
 //返回 滑动窗口中的最大值 。
 
 
+import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.LinkedList;
 
 public class _239_滑动窗口最大值 {
+
+    // 20220612 最新解法
+    // 单调队列,用一个单调队列来存储对应的下标，每当窗口滑动的时候，直接取队列的头部指针对应的值放入结果集即可
+    // 单调队列类似 （tail -->） 3 --> 2 --> 1 --> 0 (--> head) (右边为头结点，元素存的是下标)
+    // pop(value)：如果窗口移除的元素value等于单调队列的出口元素，那么队列弹出元素，否则不用任何操作
+    // push(value)：如果push的元素value大于入口元素的数值，那么就将队列入口的元素弹出，直到push元素的数值小于等于队列入口元素的数值为止
+    public int[] maxSlidingWindow2(int[] nums, int k) {
+        Deque<Integer> deque = new ArrayDeque<>();
+        int length = nums.length;
+        int[] ans = new int[length - k + 1];
+        int index = 0;
+        for (int i = 0; i < length; i++) {
+            // 根据题意，i为nums下标，是要在[i - k + 1, i] 中选到最大值，只需要保证两点
+            // 1.队列头结点需要在[i - k + 1, i]范围内，不符合则要弹出
+            while (!deque.isEmpty() && deque.peek() < i - k + 1) {
+                deque.removeFirst();
+            }
+            // 2.既然是单调，就要保证每次放进去的数字要比末尾的都大，否则也弹出
+            while (!deque.isEmpty() && nums[deque.peekLast()] < nums[i]) {
+                deque.removeLast();
+            }
+            deque.addLast(i);
+            // 因为单调，当i增长到符合第一个k范围的时候，每滑动一步都将队列头节点放入结果就行了
+            if (i >= k - 1) {
+                ans[index++] = nums[deque.peekFirst()];
+            }
+        }
+        return ans;
+    }
+
 
     public int[] maxSlidingWindow1(int[] nums, int k) {
         if (nums == null || nums.length == 0 || k == 0)
