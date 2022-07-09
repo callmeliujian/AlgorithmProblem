237a8409e7f522e4718b371ba2033c0ca3090cc5
LiuJian <liujian.nelson@bytedance.com>
2022-07-09 16:53:48 +0800

    Update _91_解码方法.java

--- src/动态规划/_91_解码方法.java
+++ src/动态规划/_91_解码方法.java
@@ -1,6 +1,64 @@
 package 动态规划;
 
+//一条包含字母 A-Z 的消息通过以下映射进行了 编码 ：
+//
+//        'A' -> "1"
+//        'B' -> "2"
+//        ...
+//        'Z' -> "26"
+//        要 解码 已编码的消息，所有数字必须基于上述映射的方法，反向映射回字母（可能有多种方法）。例如，"11106" 可以映射为：
+//
+//        "AAJF" ，将消息分组为 (1 1 10 6)
+//        "KJF" ，将消息分组为 (11 10 6)
+//        注意，消息不能分组为  (1 11 06) ，因为 "06" 不能映射为 "F" ，这是由于 "6" 和 "06" 在映射中并不等价。
+//
+//        给你一个只含数字的 非空 字符串 s ，请计算并返回 解码 方法的 总数 。
+//
+//        题目数据保证答案肯定是一个 32 位 的整数。
+//
+//         
+//
+//        示例 1：
+//
+//        输入：s = "12"
+//        输出：2
+//        解释：它可以解码为 "AB"（1 2）或者 "L"（12）。
+//        示例 2：
+//
+//        输入：s = "226"
+//        输出：3
+//        解释：它可以解码为 "BZ" (2 26), "VF" (22 6), 或者 "BBF" (2 2 6) 。
+//        示例 3：
+//
+//        输入：s = "0"
+//        输出：0
+//        解释：没有字符映射到以 0 开头的数字。
+//        含有 0 的有效映射是 'J' -> "10" 和 'T'-> "20" 。
+//        由于没有字符，因此没有有效的方法对此进行解码，因为所有数字都需要映射。
+//
+//        来源：力扣（LeetCode）
+//        链接：https://leetcode.cn/problems/decode-ways
+//        著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
 public class _91_解码方法 {
+    // https://leetcode.cn/problems/decode-ways/solution/jie-ma-fang-fa-by-leetcode-solution-p8np/
+    // 动态规划 用这个方法！
+    public int numDecodings1(String s) {
+        int length = s.length();
+        int[] dp = new int[length + 1];
+        dp[0] = 1;
+        char[] sCharArray = s.toCharArray();
+        for (int i = 1; i <= sCharArray.length; i++) {
+            if (sCharArray[i - 1] != '0') {
+                dp[i] = dp[i - 1];
+            }
+            if ((i > 1) && (sCharArray[i - 2] != '0') && ((sCharArray[i - 2] - '0') * 10 + (sCharArray[i - 1] - '0') <= 26)){
+                dp[i] += dp[i - 2];
+            }
+        }
+        return dp[length];
+    }
+
 
     public int numDecodings(String s) {
         char[] str = s.toCharArray();
