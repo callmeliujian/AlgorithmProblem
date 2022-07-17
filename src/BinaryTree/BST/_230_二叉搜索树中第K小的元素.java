2babe0bdc991fe6e9c34b3ed4b9f221f91cd4691
LiuJian <liujian.nelson@bytedance.com>
2022-07-17 23:25:26 +0800

    Update _230_二叉搜索树中第K小的元素.java

--- src/BinaryTree/BST/_230_二叉搜索树中第K小的元素.java
+++ src/BinaryTree/BST/_230_二叉搜索树中第K小的元素.java
@@ -2,43 +2,45 @@ package BinaryTree.BST;
 import common.TreeNode;
 
 public class _230_二叉搜索树中第K小的元素 {
-}
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    // 二叉搜索树中序遍历，为递增序列
-    // https://labuladong.github.io/algo/2/20/41/
-    int index = 0;
-    int ans = -1;
-    public int kthSmallest(TreeNode root, int k) {
-        traverse(root, k);
-        return ans;
-    }
-
-    private void traverse(TreeNode node, int k) {
-        if (node == null) return;
-        traverse(node.left, k);
-        index++;
-        if (index == k) {
-            ans = node.val;
-            return;
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode() {}
+     *     TreeNode(int val) { this.val = val; }
+     *     TreeNode(int val, TreeNode left, TreeNode right) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
+    class Solution {
+        // 二叉搜索树中序遍历，为递增序列
+        // https://labuladong.github.io/algo/2/20/41/
+        int index = 0;
+        int ans = -1;
+        public int kthSmallest(TreeNode root, int k) {
+            traverse(root, k);
+            return ans;
         }
 
-        traverse(node.right, k);
+        private void traverse(TreeNode node, int k) {
+            if (node == null) return;
+            traverse(node.left, k);
+            index++;
+            if (index == k) {
+                ans = node.val;
+                return;
+            }
+
+            traverse(node.right, k);
+        }
+
     }
+
 
 }
