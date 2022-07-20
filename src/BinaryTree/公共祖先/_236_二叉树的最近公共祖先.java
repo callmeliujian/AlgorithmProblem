d716007a33e600a0f014b7422f3ab497b52904b7
callmeliujian <tel.nelson@icloud.com>
2022-07-21 00:43:35 +0800

    Update _236_二叉树的最近公共祖先.java

--- src/BinaryTree/公共祖先/_236_二叉树的最近公共祖先.java
+++ src/BinaryTree/公共祖先/_236_二叉树的最近公共祖先.java
@@ -1,7 +1,36 @@
 package BinaryTree.公共祖先;
+
 import common.TreeNode;
 
 public class _236_二叉树的最近公共祖先 {
+    // https://mp.weixin.qq.com/s/njl6nuid0aalZdH5tuDpqQ?forceh5=1
+    public TreeNode lowestCommonAncestor1(TreeNode root, TreeNode p, TreeNode q) {
+        return lca(root, p.val, q.val);
+    }
+
+    private TreeNode lca(TreeNode root, int a, int b) {
+        if (root == null) return null;
+        // 此判断在前中后序都行
+        // 前绪
+        if (root.val == a || root.val == b) {
+            return root;
+        }
+        TreeNode left = lca(root.left, a, b);
+        // 中序
+        //if (root.val == a || root.val == b) {
+        //  return root;
+        //}
+        TreeNode right = lca(root.right, a, b);
+        // 后序
+        //if (root.val == a || root.val == b) {
+        //  return root;
+        //}
+        if (left != null && right != null) {
+            return root;
+        }
+        return left != null ? left : right;
+    }
+
 
     //去以 root 为根节点的二叉树中查找p、q的最近公共祖先
     //1. 如果p、q同时存在于这科二叉树中，就能成功返回它们的最近公共祖先。
@@ -14,9 +43,9 @@ public class _236_二叉树的最近公共祖先 {
             return root;
         }
         // 去以root.left为根节点的二叉树中查找p、q的最近公共祖先
-        TreeNode left = lowestCommonAncestor(root.left,p,q);
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
         // 去以root.right为根节点的二叉树中查找p、q的最近公共祖先
-        TreeNode right = lowestCommonAncestor(root.right,p,q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
         if (left != null && right != null) {
             return root;
         }
