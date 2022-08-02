ed3bdd1a7186d85f5115bb8d5285c745b89daa24
callmeliujian <tel.nelson@icloud.com>
2022-08-02 09:02:51 +0800

    Update _234_回文链表.java

--- src/LinkedList/_234_回文链表.java
+++ src/LinkedList/_234_回文链表.java
@@ -20,6 +20,89 @@ package LinkedList;
 //        著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 
 public class _234_回文链表 {
+    // 修改了原链表结构
+    class Solution1 {
+        public boolean isPalindrome(ListNode head) {
+            ListNode p = head, q = head;
+            while (p != null && p.next != null) {
+                p = p.next.next;
+                q = q.next;
+            }
+            // 链表节点数为单数，再向前走一步
+            if (p != null) {
+                q = q.next;
+            }
+
+            ListNode newHead = reverse(q);
+            ListNode node1 = newHead, node2 = head;
+            while (node1 != null && node2 != null) {
+                if (node1.val != node2.val) {
+                    return false;
+                }
+                node1 = node1.next;
+                node2 = node2.next;
+            }
+            return true;
+        }
+
+        private ListNode reverse(ListNode head) {
+            ListNode pre = null;
+            ListNode cur = head;
+            ListNode temp = null;
+            while (cur != null) {
+                temp = cur.next;
+                cur.next = pre;
+                pre = cur;
+                cur = temp;
+            }
+            return pre;
+        }
+
+    }
+
+    // 不修改原链表结构
+    // 多了这行代码 ListNode newNode = reverse(newHead);
+    class Solution2 {
+        public boolean isPalindrome(ListNode head) {
+            ListNode p = head, q = head;
+            while (p != null && p.next != null) {
+                p = p.next.next;
+                q = q.next;
+            }
+            // 链表节点数为单数，再向前走一步
+            if (p != null) {
+                q = q.next;
+            }
+
+            ListNode newHead = reverse(q);
+            ListNode node1 = newHead, node2 = head;
+            while (node1 != null && node2 != null) {
+                if (node1.val != node2.val) {
+                    return false;
+                }
+                node1 = node1.next;
+                node2 = node2.next;
+            }
+
+            ListNode newNode = reverse(newHead);
+
+            return true;
+        }
+
+        private ListNode reverse(ListNode head) {
+            ListNode pre = null;
+            ListNode cur = head;
+            ListNode temp = null;
+            while (cur != null) {
+                temp = cur.next;
+                cur.next = pre;
+                pre = cur;
+                cur = temp;
+            }
+            return pre;
+        }
+
+    }
 
     class Solution {
 
