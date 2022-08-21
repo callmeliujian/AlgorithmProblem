596f01b22be73ca91636b6b395f723dc0bdc62bf
callmeliujian <tel.nelson@icloud.com>
2022-08-21 22:01:23 +0800

    Create _剑指_Offer_53_II中缺失的数字.java

---
+++ src/Search/BinarySearch/_剑指_Offer_53_II中缺失的数字.java
@@ -0,0 +1,33 @@
+package Search.BinarySearch;
+
+public class _剑指_Offer_53_II中缺失的数字 {
+//    一个长度为n-1的递增排序数组中的所有数字都是唯一的，并且每个数字都在范围0～n-1之内。在范围0～n-1内的n个数字中有且只有一个数字不在该数组中，请找出这个数字。
+//
+//
+//
+//    示例 1:
+//
+//    输入: [0,1,3]
+//    输出: 2
+//    示例 2:
+//
+//    输入: [0,1,2,3,4,5,6,7,9]
+//    输出: 8
+
+    class Solution {
+        // https://leetcode.cn/problems/que-shi-de-shu-zi-lcof/solution/mian-shi-ti-53-ii-0n-1zhong-que-shi-de-shu-zi-er-f/
+        public int missingNumber(int[] nums) {
+            int left = 0, right = nums.length - 1;
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] == mid) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+            return left;
+        }
+    }
+
+}
