638dc88d8a93e391d8f3eb8c1375e51712441313
callmeliujian <tel.nelson@icloud.com>
2022-10-17 09:28:44 +0800

    Update 面试题_01_09_字符串轮转.java

--- src/String/面试题_01_09_字符串轮转.java
+++ src/String/面试题_01_09_字符串轮转.java
@@ -19,7 +19,7 @@ public class 面试题_01_09_字符串轮转 {
 //    public boolean isFlipedString(String s1, String s2) {
 //        return s1.length() == s2.length() && (s1 + s1).contains(s2);
 //    }
-}
+//}
 
     public boolean isFlipedString(String s1, String s2) {
         if (s1 == null || s2 == null)
